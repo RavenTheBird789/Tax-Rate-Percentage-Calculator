d35143fa606fd7329db88b5d7838d5a17436be66
Art3mis_XP <northwalker676@gmail.com>
2025-08-19 17:28:22 -0400

    Add files via upload

---
+++ tax_rate.py
@@ -0,0 +1,13 @@
+# Tax Rate Percentage Evaluation Program
+
+def tax_rate():
+    initial_price = float(input("Initial Price: ")); # ask user for listed price of something
+    final_price = float(input("Final Price: ")); # ask user the final price paid for something after tax
+
+    diff = final_price - initial_price # evaluate the difference between the final price after tax and the price listed
+
+    formula = diff / initial_price * 100 # evaluate the tax percentage using this formula
+    
+    mes = f"The current tax rate based on your purchase is {formula}"
+    print(mes); # show the user the current tax rate based on their purchase
+tax_rate();

e564b2678fb0f1e7d602c56943a2d9cdaa1251c1
Art3mis_XP <northwalker676@gmail.com>
2025-11-20 06:42:17 -0500

    Update tax_rate.py

--- tax_rate.py
+++ tax_rate.py
@@ -1,8 +1,8 @@
 # Tax Rate Percentage Evaluation Program
 
 def tax_rate():
-    initial_price = float(int(input("Initial Price: "))); # ask user for listed price of something
-    final_price = float(int(input("Final Price: "))); # ask user the final price paid for something after tax
+    initial_price = int(float(input("Initial Price: "))); # ask user for listed price of something
+    final_price = int(float(input("Final Price: "))); # ask user the final price paid for something after tax
 
     diff = final_price - initial_price # evaluate the difference between the final price after tax and the price listed
 
